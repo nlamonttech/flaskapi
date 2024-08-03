a046a041040bfe0503e4a604c7be9da5132ac910
neil.lamont <nlamonttechguy@gmx.com>
2024-08-03 20:19:32 +0100

    Go

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,67 +1,510 @@
 provider "aws" {
+    region = "eu-west-1"
+}
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.46"
+    }
+  }
+}
+
+locals {
+  #account_id = "${data.aws_caller_identity.current.account_id}"
+  account_id = var.aws_account_id
+  name   = "ex-${basename(path.cwd)}"
   region = "eu-west-1"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-vpc"
+    GithubOrg  = "terraform-aws-modules"
+  }
 }
 
-# Retrieve IAM policy documents for assuming roles.  This is used to feed exclusive inline policies.
 
-# Create an IAM policy document for assumed roles for ECS service.
-data "aws_iam_policy_document" "ecs_service_role" {
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.12.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs                 = local.azs
+  private_subnets     = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets      = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+  database_subnets    = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 8)]
+  elasticache_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 12)]
+  redshift_subnets    = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 16)]
+  intra_subnets       = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 20)]
+
+  private_subnet_names = ["Private Subnet One", "Private Subnet Two"]
+  # public_subnet_names omitted to show default name generation for all three subnets
+  database_subnet_names    = ["DB Subnet One"]
+  elasticache_subnet_names = ["Elasticache Subnet One", "Elasticache Subnet Two"]
+  redshift_subnet_names    = ["Redshift Subnet One", "Redshift Subnet Two", "Redshift Subnet Three"]
+  intra_subnet_names       = []
+
+  create_database_subnet_group  = false
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  customer_gateways = {
+    IP1 = {
+      bgp_asn     = 65112
+      ip_address  = "1.2.3.4"
+      device_name = "some_name"
+    },
+    IP2 = {
+      bgp_asn    = 65112
+      ip_address = "5.6.7.8"
+    }
+  }
+
+  enable_vpn_gateway = true
+
+  enable_dhcp_options              = true
+  dhcp_options_domain_name         = "service.consul"
+  dhcp_options_domain_name_servers = ["127.0.0.1", "10.10.0.2"]
+
+  # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
+  vpc_flow_log_iam_role_name            = "vpc-complete-example-role"
+  vpc_flow_log_iam_role_use_name_prefix = false
+  enable_flow_log                       = true
+  create_flow_log_cloudwatch_log_group  = true
+  create_flow_log_cloudwatch_iam_role   = true
+  flow_log_max_aggregation_interval     = 60
+
+  tags = local.tags
+}
+
+################################################################################
+# VPC Endpoints Module
+################################################################################
+
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "5.12.0"
+
+  vpc_id = module.vpc.vpc_id
+
+  create_security_group      = true
+  security_group_name_prefix = "${local.name}-vpc-endpoints-"
+  security_group_description = "VPC endpoint security group"
+  security_group_rules = {
+    ingress_https = {
+      description = "HTTPS from VPC"
+      cidr_blocks = [module.vpc.vpc_cidr_block]
+    }
+  }
+
+  endpoints = {
+    s3 = {
+      service             = "s3"
+      private_dns_enabled = true
+      dns_options = {
+        private_dns_only_for_inbound_resolver_endpoint = false
+      }
+      tags = { Name = "s3-vpc-endpoint" }
+    },
+    dynamodb = {
+      service         = "dynamodb"
+      service_type    = "Gateway"
+      route_table_ids = flatten([module.vpc.intra_route_table_ids, module.vpc.private_route_table_ids, module.vpc.public_route_table_ids])
+      policy          = data.aws_iam_policy_document.dynamodb_endpoint_policy.json
+      tags            = { Name = "dynamodb-vpc-endpoint" }
+    },
+    ecs = {
+      service             = "ecs"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    ecs_telemetry = {
+      create              = false
+      service             = "ecs-telemetry"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+    },
+    ecr_api = {
+      service             = "ecr.api"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      policy              = data.aws_iam_policy_document.generic_endpoint_policy.json
+    },
+    ecr_dkr = {
+      service             = "ecr.dkr"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      policy              = data.aws_iam_policy_document.generic_endpoint_policy.json
+    },
+    rds = {
+      service             = "rds"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      security_group_ids  = [aws_security_group.rds.id]
+    },
+  }
+
+  tags = merge(local.tags, {
+    Project  = "Secret"
+    Endpoint = "true"
+  })
+}
+
+module "vpc_endpoints_nocreate" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "5.12.0"
+
+  create = false
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+data "aws_iam_policy_document" "dynamodb_endpoint_policy" {
   statement {
-    actions = ["sts:AssumeRole"]
+    effect    = "Deny"
+    actions   = ["dynamodb:*"]
+    resources = ["*"]
 
     principals {
-      type        = "Service"
-      identifiers = ["ecs.amazonaws.com", "ec2.amazonaws.com", "ecs-tasks.amazonaws.com"]
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:sourceVpc"
+
+      values = [module.vpc.vpc_id]
     }
   }
 }
 
-# Get the AccountId
-data "aws_caller_identity" "current" {}
-
-# Create an IAM policy document for assumed roles for EC2.
-data "aws_iam_policy_document" "ec2_role" {
+data "aws_iam_policy_document" "generic_endpoint_policy" {
   statement {
-    actions = ["sts:AssumeRole"]
+    effect    = "Deny"
+    actions   = ["*"]
+    resources = ["*"]
 
     principals {
-      type        = "Service"
-      identifiers = ["ecs.amazonaws.com", "ec2.amazonaws.com", "dynamodb.amazonaws.com", "ecs-tasks.amazonaws.com"]
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringNotEquals"
+      variable = "aws:SourceVpc"
+
+      values = [module.vpc.vpc_id]
     }
   }
 }
 
-# Create an IAM policy document for assumed roles for EC2 autoscaling.
-data "aws_iam_policy_document" "autoscaling" {
-  statement {
-    actions = ["sts:AssumeRole"]
+resource "aws_security_group" "rds" {
+  name_prefix = "${local.name}-rds"
+  description = "Allow PostgreSQL inbound traffic"
+  vpc_id      = module.vpc.vpc_id
 
-    principals {
-      type        = "Service"
-      identifiers = ["application-autoscaling.amazonaws.com"]
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  tags = local.tags
+}
+
+
+
+# Create a security group for the ALB.
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-sg"
+  description = "ECS security group for the ALB."
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 31000
+    to_port   = 61000
+    self      = true
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create a CloudWatch log group.
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "ecs-logs"
+  retention_in_days = 14
+}
+
+# Create an ECS task definition.
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+  family                = "${var.service_name}-ecs-demo-app"
+  container_definitions = <<DEFINITION
+[
+  {
+    "name": "demo-app",
+    "cpu": 10,
+    "image": "${var.ecs_image_url}",
+    "essential": true,
+    "memory": 300,
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "ecs-logs",
+        "awslogs-region": "us-east-1",
+        "awslogs-stream-prefix": "ecs-demo-app"
+      }
+    },
+    "mountPoints": [
+      {
+        "containerPath": "/usr/local/apache2/htdocs",
+        "sourceVolume": "my-vol"
+      }
+    ],
+    "portMappings": [
+      {
+        "containerPort": 5000
+      }
+    ]
+  }
+]
+DEFINITION
+  volume {
+    name = "my-vol"
+  }
+}
+
+# Create the Application Load Balancer.
+resource "aws_lb" "main" {
+  name                       = "ecsalb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.ecs_sg.id]
+  subnets                    = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  idle_timeout               = 30
+  enable_deletion_protection = false
+}
+
+# Create the ALB target group.
+resource "aws_lb_target_group" "ecs_rest_api_tg" {
+  name     = "ecs-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 2
+    interval            = 10
+    matcher             = "200"
+  }
+}
+
+# Create the ALB listener.
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    target_group_arn = aws_lb_target_group.ecs_rest_api_tg.arn
+    type             = "forward"
+  }
+}
+
+# Create the ECS cluster.
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "ecs_cluster"
+}
+
+# Create the ECS service.
+resource "aws_ecs_service" "service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+  desired_count   = var.desired_capacity
+  iam_role        = aws_iam_role.ecs_service_role.arn
+  depends_on      = [aws_lb_listener.alb_listener]
+  load_balancer {
+    container_name   = "demo-app"
+    container_port   = 5000
+    target_group_arn = aws_lb_target_group.ecs_rest_api_tg.arn
+  }
+}
+
+# Create an EC2 instance profile.
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+# Create an EC2 Launch Configuration for the ECS cluster.
+resource "aws_launch_configuration" "ecs_launch_config" {
+  image_id             = data.aws_ami.latest_ecs_ami.image_id
+  security_groups      = [aws_security_group.ecs_sg.id]
+  instance_type        = var.instance_type
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+  user_data            = "#!/bin/bash\necho ECS_CLUSTER=ecs_cluster >> /etc/ecs/ecs.config"
+}
+
+# Create the ECS autoscaling group.
+resource "aws_autoscaling_group" "ecs_asg" {
+  name                 = "ecs-asg"
+  vpc_zone_identifier  = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  launch_configuration = aws_launch_configuration.ecs_launch_config.name
+
+  desired_capacity = var.desired_capacity
+  min_size         = 1
+  max_size         = var.maximum_capacity
+}
+
+# Create an autoscaling policy.
+resource "aws_autoscaling_policy" "ecs_infra_scale_out_policy" {
+  name                   = "ecs_infra_scale_out_policy"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  autoscaling_group_name = aws_autoscaling_group.ecs_asg.name
+}
+
+# Create an application autoscaling target.
+resource "aws_appautoscaling_target" "ecs_service_scaling_target" {
+  max_capacity       = 5
+  min_capacity       = 2
+  resource_id        = "service/${aws_ecs_cluster.ecs_cluster.name}/${var.service_name}"
+  role_arn           = aws_iam_role.autoscaling_role.arn
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+  depends_on         = [aws_ecs_service.service]
+}
+
+# Create an ECS service CPU target tracking scale out policy.
+resource "aws_appautoscaling_policy" "ecs_service_cpu_scale_out_policy" {
+  name               = "cpu-target-tracking-scaling-policy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_service_scaling_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_service_scaling_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_service_scaling_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
+
+    target_value       = 50.0
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
   }
 }
 
-# Get the latest AMI ID for the ECS optimized Amazon Linux 2 image.
-data "aws_ami" "latest_ecs_ami" {
-  most_recent = true
+# Create an ECS service memory target tracking scale out policy.
+resource "aws_appautoscaling_policy" "ecs_service_memory_scale_out_policy" {
+  name               = "memory-target-tracking-scaling-policy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_service_scaling_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_service_scaling_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_service_scaling_target.service_namespace
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-ecs-*"]
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = 50.0
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  owners = ["amazon"]
 }
 
+# Create a CloudWatch alarm for ECS service CPU scale out.
+resource "aws_cloudwatch_metric_alarm" "ecs_service_cpu_scale_out_alarm" {
+  alarm_name          = "CPU utilization greater than 50%"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "50"
+  alarm_description   = "Alarm if CPU utilization is greater than 50% of reserved CPU"
+  dimensions = {
+    "Name"  = "ClusterName"
+    "Value" = aws_ecs_cluster.ecs_cluster.name
+  }
+  alarm_actions = [aws_appautoscaling_policy.ecs_service_cpu_scale_out_policy.arn]
+}
+
+# Create a CloudWatch alarm for ECS service CPU scale out.
+resource "aws_cloudwatch_metric_alarm" "ecs_infra_cpu_alarm_high" {
+  alarm_name          = "CPU utilization greater than 50%"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "10"
+  alarm_description   = "Alarm if CPU too high or metric disappears indicating instance is down"
+  dimensions = {
+    "Name"  = "AutoScalingGroupName"
+    "Value" = aws_autoscaling_group.ecs_asg.name
+  }
+  alarm_actions = [aws_autoscaling_policy.ecs_infra_scale_out_policy.arn]
+}
+
+# Create a DynamoDB table.
+resource "aws_dynamodb_table" "music_table" {
+  name           = var.dynamodb_table_name
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "artist"
+
+  attribute {
+    name = "artist"
+    type = "S"
+  }
+}
